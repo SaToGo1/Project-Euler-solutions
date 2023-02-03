f2004be41916b95d6eeb247772ebb246fd85d878
SaToGo <stoledano97@gmail.com>
2023-02-03 22:47:34 +0100

    problem 4 solved

--- javascript/p0004.js
+++ javascript/p0004.js
@@ -1,15 +1,30 @@
-const isPalindrome = (string) => {
-    console.log(string)
-    let j = string.length - 1;
-    for(let i = 0; i<string.length; i++){
-        if(string[i] != string[j]) return false;
+const START_3_DIGIT = 100;
+const FINISH_3_DIGIT = 999;
+
+const START_2_DIGIT = 10;
+const FINISH_2_DIGIT = 99;
+
+const isPalindrome = (num) => {
+    str = num.toString();
+    let j = str.length - 1;
+    for(let i = 0; i < str.length; i++){
+        if(str[i] != str[j]) return false;
         j--;
     }
     return true;
 }
 
+const largestPalindromeFromProduct = (numStart, numStart2, numFinish, numFinish2) => {
+    let largestPalindrome = 0;
+    for(let i = numStart; i <= numFinish; i++){
+        for(let j = numStart2; j <= numFinish2; j++){
+            let product = i * j;
+            if(isPalindrome(product)){
+                if(product > largestPalindrome) largestPalindrome = product;
+            }
+        }
+    }
+    return largestPalindrome;
+}
 
-console.log(`false = ${isPalindrome('abcd')}`);
-console.log(`true = ${isPalindrome('abba')}`);
-console.log(`false = ${isPalindrome('abcabc')}`);
-console.log(`true = ${isPalindrome('abcba')}`);
+console.log(largestPalindromeFromProduct(START_3_DIGIT, START_3_DIGIT, FINISH_3_DIGIT, FINISH_3_DIGIT));
